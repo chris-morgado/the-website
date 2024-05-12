d7285f2f53017d185c32417bdce0593d45a003e6
Christian <cmmorgado53@gmail.com>
2024-05-11 23:41:57 -0400

    Back to basics

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,15 @@ import '../styles/globals.css';
 
 export default function Home() {
     return (
-            // <div className="terminal">
-            //     <div className="terminal-content">
-            //         <h1>test so far</h1>
-            //         <p>I'm christian, and this is my personal website :)</p>
-            //         <nav>
-            //             <a href="#projects">Projects</a> | <a href="#about">About</a> | <a href="#contact">Contact</a> | <a href="#contact">Poop</a>
-            //         </nav>
-            //     </div>
-            // </div>
-        <WebGLCanvas></WebGLCanvas>
+            <div className="terminal">
+                <div className="terminal-content">
+                    <h1>LOOKING GOOD!!!</h1>
+                    <p>I'm christian, and this is my personal website :)</p>
+                    <nav>
+                        <a href="#projects">Projects</a> | <a href="#about">About</a> | <a href="#contact">Contact</a> | <a href="#contact">Poop</a>
+                    </nav>
+                </div>
+            </div>
+        // <WebGLCanvas></WebGLCanvas>
     );
 }
