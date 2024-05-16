6e9b56493fe3dd9a3bab8cf839b4b63e69d2e79a
Christian <cmmorgado53@gmail.com>
2024-05-16 14:43:16 -0400

    Edited navitems

--- components/nav/navitems.jsx
+++ components/nav/navitems.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function NavItems() {
     const navItems = [
         { name: "Home", link: "/" },
-        { name: "About", link: "/about" },
-        { name: "Services", link: "/services" },
-        { name: "Contact", link: "/contact" }
+        { name: "Projects", link: "/projects" },
+        { name: "Contact", link: "/contact" },
+        { name: "Terminal", link: "/terminal" }
     ];
     
     return (
