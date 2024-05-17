7016baca34442fbfc355509a05075825c7ee0756
Christian <cmmorgado53@gmail.com>
2024-05-16 20:08:28 -0400

    Navbar element

--- components/nav/navbars.jsx
+++ components/nav/navbars.jsx
@@ -1,7 +1,42 @@
-import NavItems from "./navitems";
+import Link from "next/link";
 
 export default function MainNavBar() {
+    const leftNavdata = [
+        { name: "Home", link: "/" },
+    ]
+    
+    const navData = [
+		{ name: "Projects", link: "/projects" },
+		{ name: "Contact", link: "/contact" },
+		{ name: "Terminal", link: "/terminal" }
+	];
+    
     return (
-        <NavItems></NavItems>
+        <div>
+            {navData.map((item, index) => (
+                <div id={`item-${index}`}>
+                    <Link key={index} href={item.link}>{item.name}</Link>
+                </div>
+            ))}    
+        </div>
+    );
+}
+
+export function TerminalNavBar() {
+    const leftNavData = [
+        { name: "Terminal", link: "/terminal" },
+    ]
+    
+    const navData = [
+		{ name: "About", link: "/terminal/about" },
+		{ name: "Command Line", link: "/terminal/cmd" },
+		{ name: "Games", link: "/terminal/games" },
+		{ name: "Exit Terminal", link: "/" }
+	];
+    
+    return (
+        <>
+    
+        </>
     );
 }
