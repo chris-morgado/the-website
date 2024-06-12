b14f5e264daae6b388171a8f8d88084e4e795dc3
Christian <cmmorgado53@gmail.com>
2024-06-12 02:41:34 -0400

    Added projects and project schema

--- next/app/projects/ProjectSchema.tsx
+++ next/app/projects/ProjectSchema.tsx
@@ -1,10 +1,22 @@
 type Project = {
     id: number;
     slug: string;
-    name: string;
-    description: string;
-    start_date: string;
-    end_date: string;
+    title: string;
+    text: string;
+    start_date: Date;
+    end_date: Date | null;
+    post_date: Date;
     completed: boolean;
-    tags?: string[]; 
+    // tags?: string[]; 
+};
+
+const exampleProject: Project = {
+    id: 1,
+    slug: "/example",
+    title: "Sample Project",
+    text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+    start_date: new Date("2023-01-16"), // Jan 16, 2023, Will be shown as Jan. 2023
+    end_date: new Date("2023-02-06"),   // Feb 16, 2023, Will be shown as Feb. 2023
+    post_date: new Date("2024-06-12"),
+    completed: true
 };
---
+++ next/app/projects/Projects.tsx
@@ -0,0 +1,123 @@
+{/* 
+Projects:
+- TestTail <-- WIP
+- Rush Hour <-- WIP
+- CircuitCheatSheet <-- WIP
+- Club App <-- WIP
+- U-Fund Application <-- WIP
+- M.U.D. <-- WIP
+- Coffee Minecraft Mod <-- WIP
+- DegreeMap <-- WIP
+- This Website! <-- WIP
+*/}
+const projects: Project[] = [
+    {
+        id: 9,
+        slug: "/thewebsite",
+        title: "TheWebsite",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2024-06-15"), 
+        end_date: null,   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+    
+    {
+        id: 9,
+        slug: "/degreemap",
+        title: "DegreeMap",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2024-04-15"), 
+        end_date: null,   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+
+    {
+        id: 8,
+        slug: "/coffeemod",
+        title: "StretchyCoffee -- Minecraft Mod",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2024-03-15"), 
+        end_date: null,   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+
+    {
+        id: 7,
+        slug: "/mud",
+        title: "Multi-User Dungeon -- 2D Roguelike",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2024-01-15"), 
+        end_date: new Date("2024-05-15"),   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+
+    {
+        id: 6,
+        slug: "/clubapp",
+        title: "Club Attendance Tracker Full Stack Web App",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2023-10-15"), 
+        end_date: new Date("2023-12-15"),   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+
+    {
+        id: 5,
+        slug: "/ssequel",
+        title: "SSE Website Redesign",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2023-08-15"), 
+        end_date: null,   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+    
+    {
+        id: 4,
+        slug: "/ufund",
+        title: "uFund Sam & Friends Full Stack Web App",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2023-08-15"), 
+        end_date: new Date("2023-12-15"),   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+
+    {
+        id: 3,
+        slug: "/circuit1",
+        title: "CircuitCheatSheet",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2023-05-15"), 
+        end_date: null,   
+        post_date: new Date("2024-06-12"),
+        completed: false
+    },
+    
+    {
+        id: 2,
+        slug: "/rushhour",
+        title: "Rush Hour Java Applet",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2023-03-15"), 
+        end_date: new Date("2023-05-15"),   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    },
+    
+    {
+        id: 1,
+        slug: "/testtail",
+        title: "TestTail",
+        text: "<p>This is a <strong>sample</strong> project with HTML content.</p>",
+        start_date: new Date("2022-08-15"), 
+        end_date: new Date("2022-12-15"),   
+        post_date: new Date("2024-06-12"),
+        completed: true
+    }
+]
