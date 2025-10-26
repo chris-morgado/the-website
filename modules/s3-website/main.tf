6a95f3bf8ad749ea3a4ff59ece2d1b6df70159cf
Chris Morgado <cmmorgado53@gmail.com>
2025-10-26 16:38:31 -0400

    formatted all terraform files

--- modules/s3-website/main.tf
+++ modules/s3-website/main.tf
@@ -19,14 +19,14 @@ resource "aws_s3_bucket_policy" "oac_read" {
   bucket = aws_s3_bucket.site.id
 
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "AllowCloudFrontReadViaOAC"
-        Effect   = "Allow"
+        Sid       = "AllowCloudFrontReadViaOAC"
+        Effect    = "Allow"
         Principal = { Service = "cloudfront.amazonaws.com" }
-        Action   = ["s3:GetObject"]
-        Resource = ["${aws_s3_bucket.site.arn}/*"]
+        Action    = ["s3:GetObject"]
+        Resource  = ["${aws_s3_bucket.site.arn}/*"]
         Condition = {
           StringEquals = {
             "AWS:SourceArn" : var.cloudfront_distribution_arn
--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "aws_region" {
-	type    = string
-	default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "bucket_name" {
-	type    = string
-	default = "chrismorgados-the-website"
+  type    = string
+  default = "chrismorgados-the-website"
 }
 
 variable "index_html_content" {
-	type    = string
-	default = "<!doctype html><html><head><meta charset='utf-8'><title>It works</title></head><body><h1>It works 🎉</h1></body></html>"
+  type    = string
+  default = "<!doctype html><html><head><meta charset='utf-8'><title>It works</title></head><body><h1>It works 🎉</h1></body></html>"
 }
-variable "domain_root"            { 
-    type = string 
-    default = "chrismorgado.com" 
+variable "domain_root" {
+  type    = string
+  default = "chrismorgado.com"
 }
-variable "domain_www"             { 
-    type = string 
-    default = "www.chrismorgado.com" 
+variable "domain_www" {
+  type    = string
+  default = "www.chrismorgado.com"
 }
-variable "cloudflare_zone_name"   { 
-    type = string 
-    default = "chrismorgado.com" 
+variable "cloudflare_zone_name" {
+  type    = string
+  default = "chrismorgado.com"
 }
-variable "cloudflare_api_token"   { 
-    type = string 
+variable "cloudflare_api_token" {
+  type = string
 }
-variable "cloudflare_zone_id" { 
-    type = string
-    default = "323449e26ea5be6bbdcddd84159e6882"
+variable "cloudflare_zone_id" {
+  type    = string
+  default = "323449e26ea5be6bbdcddd84159e6882"
 }
