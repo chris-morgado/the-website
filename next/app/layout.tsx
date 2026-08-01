a8342d8ebf1e580914eadf95ee8efb1095799ef2
Chris Morgado <cmmorgado53@gmail.com>
2026-08-01 13:32:14 -0400

    changed font to Rethink Sans

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import "../styles/globals.css"; 
+import "../styles/globals.css";
 import ClientReady from '../components/ClientReady';
+import { Rethink_Sans } from 'next/font/google';
+
+const rethinkSans = Rethink_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  style: ['normal', 'italic'],
+  variable: '--font-rethink',
+});
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -8,7 +16,7 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className={`${rethinkSans.variable} scroll-smooth`}>
       <body>
         {/* flips html.page-ready on hydration */}
         <ClientReady />
